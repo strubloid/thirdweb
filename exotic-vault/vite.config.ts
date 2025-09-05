f158bee5bdc481e1f30162e7598b448aea01a558
Rafael Mendes <joserafaelmb@gmail.com>
2025-09-05 11:41:10 +0100

    adding the option to see in another machine

--- exotic-vault/vite.config.ts
+++ exotic-vault/vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: process.env.VITE_HOST || '0.0.0.0',
+    port: parseInt(process.env.VITE_PORT || '5173'),
+    // only run hmr if we have set the vite_host
+    ...(process.env.VITE_HOST && {
+      hmr: { 
+        host: process.env.VITE_HOST,
+      },
+    }),
+  },
 });
